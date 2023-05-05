acc54e284ca1cf9c17f3513969b8d04a3c1336ec
lucidwrld <kabiawubenedict@gmail.com>
2023-05-05 20:33:49 +0100

    werwe

---
+++ Homebiz.js
@@ -0,0 +1,434 @@
+let searchfa = document.querySelector(".searchfa")
+let timesfa = document.querySelector(".timesfa")
+let searchover = document.querySelector(".searchover")
+
+const optionsList = document.querySelectorAll(".option");
+searchfa.addEventListener('click', function () {
+ console.log("sdasd")
+ searchover.style.transform = "translate(0%)"
+})
+timesfa.addEventListener('click', function () {
+ console.log("sdasd")
+ searchover.style.transform = "translateY(-100%)"
+})
+
+const dropdownToggle = document.getElementById('dropdownMenuButton');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownSearch = document.getElementById('dropdown-search');
+const dropdownOptions = document.querySelectorAll('.dropdown-item');
+
+dropdownToggle.addEventListener('click', () => {
+ dropdownMenu.style.display = ""
+ dropdownMenu.classList.toggle('show');
+ dropdownMenuc.style.display = "none"
+ console.log("sdas")
+});
+
+dropdownSearch.addEventListener('input', () => {
+ const searchValue = dropdownSearch.value.toLowerCase();
+ for (let i = 0; i < dropdownOptions.length; i++) {
+  const option = dropdownOptions[i];
+  const text = option.textContent.toLowerCase();
+  if (text.includes(searchValue)) {
+   option.style.display = "";
+  } else {
+   option.style.display = "none";
+  }
+ }
+});
+
+
+const dropdownTogglec = document.getElementById('dropdownMenuButtonc');
+const dropdownMenuc = document.querySelector('.dropdown-menuc');
+const dropdownSearchc = document.getElementById('dropdown-searchc');
+const dropdownOptionsc = document.querySelectorAll('.dropdown-itemc');
+
+dropdownTogglec.addEventListener('click', () => {
+ dropdownMenuc.style.display = ""
+ dropdownMenuc.classList.toggle('show');
+ dropdownMenu.style.display = "none"
+ console.log("sdas")
+});
+
+dropdownSearchc.addEventListener('input', () => {
+ const searchValue = dropdownSearchc.value.toLowerCase();
+ for (let i = 0; i < dropdownOptionsc.length; i++) {
+  const option = dropdownOptionsc[i];
+  const text = option.textContent.toLowerCase();
+  if (text.includes(searchValue)) {
+   option.style.display = "";
+  } else {
+   option.style.display = "none";
+  }
+ }
+});
+
+
+/*mainpage toggling*/
+
+const dropdownTogglem = document.getElementById('dropdownMenuButtonm');
+const dropdownMenum = document.querySelector('.dropdown-menum');
+const dropdownSearchm = document.getElementById('dropdown-searchm');
+const dropdownOptionsm = document.querySelectorAll('.dropdown-itemm');
+
+dropdownTogglem.addEventListener('click', () => {
+ dropdownMenum.style.display = ""
+ dropdownMenum.classList.toggle('show');
+ dropdownMenumm.style.display = "none"
+ console.log("sdas")
+});
+
+dropdownSearchm.addEventListener('input', () => {
+ const searchValue = dropdownSearchm.value.toLowerCase();
+ for (let i = 0; i < dropdownOptionsm.length; i++) {
+  const option = dropdownOptionsm[i];
+  const text = option.textContent.toLowerCase();
+  if (text.includes(searchValue)) {
+   option.style.display = "";
+  } else {
+   option.style.display = "none";
+  }
+ }
+});
+
+
+const dropdownTogglemm = document.getElementById('dropdownMenuButtonmm');
+const dropdownMenumm = document.querySelector('.dropdown-menumm');
+const dropdownSearchmm = document.getElementById('dropdown-searchmm');
+const dropdownOptionsmm = document.querySelectorAll('.dropdown-itemmm');
+
+dropdownTogglemm.addEventListener('click', () => {
+ dropdownMenumm.style.display = ""
+ dropdownMenumm.classList.toggle('show');
+ dropdownMenum.style.display = "none"
+ console.log("sdas")
+});
+
+dropdownSearchmm.addEventListener('input', () => {
+ const searchValue = dropdownSearchmm.value.toLowerCase();
+ for (let i = 0; i < dropdownOptionsmm.length; i++) {
+  const option = dropdownOptionsmm[i];
+  const text = option.textContent.toLowerCase();
+  if (text.includes(searchValue)) {
+   option.style.display = "";
+  } else {
+   option.style.display = "none";
+  }
+ }
+});
+const filterdrop = document.getElementById('filterdrop');
+const filterdropmenu = document.querySelector('.fildrop');
+filterdrop.addEventListener('click', () => {
+ filterdropmenu.classList.toggle('show');
+ console.log("sdas")
+});
+const menu = document.getElementById('tab');
+const dropmen = document.querySelector('.tabdrop');
+const ttimesfa = document.querySelector(".ttimesfa")
+menu.addEventListener('click', () => {
+ dropmen.style.transform = "translateX(0%)"
+ console.log("sdas")
+});
+ttimesfa.addEventListener('click', function () {
+ console.log("sdasd")
+ dropmen.style.transform = "translateX(-100%)"
+})
+
+
+
+const showbtn = document.getElementById('showbtn');
+const tagmenu = document.querySelector('.chek');
+const tagmen = document.querySelector('.tAG');
+showbtn.addEventListener('click', () => {
+ if (showbtn.innerHTML = `Show More <i class="fa fa-arrow-up"></i>`) {
+  console.log("sdnajs")
+  tagmen.style.height = "1080px"
+  tagmenu.style.height = "1020px"
+  tagmenu.innerHTML = `<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Designs</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Paints</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Designs</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Paints</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Designs</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Paints</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div>
+       <div class="holdcheck">
+        <input class="checkbox" type="checkbox" name="" id="">
+        <label class="fw-semibold" for="">3D Panel</label>
+       </div><div class="holdcheck">
+       <input class="checkbox" type="checkbox" name="" id="">
+       <label class="fw-semibold" for="">3D Designs</label>
+      </div>
+      <div class="holdcheck">
+       <input class="checkbox" type="checkbox" name="" id="">
+       <label class="fw-semibold" for="">3D Paints</label>
+      </div>
+      <div class="holdcheck">
+       <input class="checkbox" type="checkbox" name="" id="">
+       <label class="fw-semibold" for="">3D Panel</label>
+      </div>
+      <div class="holdcheck">
+       <input class="checkbox" type="checkbox" name="" id="">
+       <label class="fw-semibold" for="">3D Panel</label>
+      </div><div class="holdcheck">
+      <input class="checkbox" type="checkbox" name="" id="">
+      <label class="fw-semibold" for="">3D Designs</label>
+     </div>
+     <div class="holdcheck">
+      <input class="checkbox" type="checkbox" name="" id="">
+      <label class="fw-semibold" for="">3D Paints</label>
+     </div>
+     <div class="holdcheck">
+      <input class="checkbox" type="checkbox" name="" id="">
+      <label class="fw-semibold" for="">3D Panel</label>
+     </div>
+     <div class="holdcheck">
+      <input class="checkbox" type="checkbox" name="" id="">
+      <label class="fw-semibold" for="">3D Panel</label>
+     </div><div class="holdcheck">
+     <input class="checkbox" type="checkbox" name="" id="">
+     <label class="fw-semibold" for="">3D Designs</label>
+    </div>
+    <div class="holdcheck">
+     <input class="checkbox" type="checkbox" name="" id="">
+     <label class="fw-semibold" for="">3D Paints</label>
+    </div>
+    <div class="holdcheck">
+     <input class="checkbox" type="checkbox" name="" id="">
+     <label class="fw-semibold" for="">3D Panel</label>
+    </div>
+    <div class="holdcheck">
+     <input class="checkbox" type="checkbox" name="" id="">
+     <label class="fw-semibold" for="">3D Panel</label>
+    </div><div class="holdcheck">
+    <input class="checkbox" type="checkbox" name="" id="">
+    <label class="fw-semibold" for="">3D Designs</label>
+   </div>
+   <div class="holdcheck">
+    <input class="checkbox" type="checkbox" name="" id="">
+    <label class="fw-semibold" for="">3D Paints</label>
+   </div>
+   <div class="holdcheck">
+    <input class="checkbox" type="checkbox" name="" id="">
+    <label class="fw-semibold" for="">3D Panel</label>
+   </div>
+   <div class="holdcheck">
+    <input class="checkbox" type="checkbox" name="" id="">
+    <label class="fw-semibold" for="">3D Panel</label>
+   </div><div class="holdcheck">
+   <input class="checkbox" type="checkbox" name="" id="">
+   <label class="fw-semibold" for="">3D Designs</label>
+  </div>
+  <div class="holdcheck">
+   <input class="checkbox" type="checkbox" name="" id="">
+   <label class="fw-semibold" for="">3D Paints</label>
+  </div>
+  <div class="holdcheck">
+   <input class="checkbox" type="checkbox" name="" id="">
+   <label class="fw-semibold" for="">3D Panel</label>
+  </div>
+  <div class="holdcheck">
+   <input class="checkbox" type="checkbox" name="" id="">
+   <label class="fw-semibold" for="">3D Panel</label>
+  </div><div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Designs</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Paints</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Panel</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Panel</label>
+ </div><div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+ <input class="checkbox" type="checkbox" name="" id="">
+ <label class="fw-semibold" for="">3D Panel</label>
+</div><div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div><div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div><div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div><div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div><div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Designs</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Paints</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+<div class="holdcheck">
+<input class="checkbox" type="checkbox" name="" id="">
+<label class="fw-semibold" for="">3D Panel</label>
+</div>
+ `
+  showbtn.innerHTML = `Show Less <i class="fa fa-arrow-up"></i>`
+ } else if (showbtn.innerHTML = `Show Less <i class="fa fa-arrow-up"></i>`) {
+  console.log("sdnajs")
+  tagmen.style.height = "80px"
+  tagmenu.style.height = "20px"
+  tagmenu.innerHTML = `<div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Designs</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Paints</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Panel</label>
+ </div>
+ <div class="holdcheck">
+  <input class="checkbox" type="checkbox" name="" id="">
+  <label class="fw-semibold" for="">3D Panel</label>
+ </div>
+ <div class="holdcheck">
+         <input class="checkbox" type="checkbox" name="" id="">
+         <label class="fw-semibold" for="">3D Designs</label>
+        </div>
+        <div class="holdcheck">
+         <input class="checkbox" type="checkbox" name="" id="">
+         <label class="fw-semibold" for="">3D Paints</label>
+        </div>
+        <div class="holdcheck">
+         <input class="checkbox" type="checkbox" name="" id="">
+         <label class="fw-semibold" for="">3D Panel</label>
+        </div>
+        <div class="holdcheck">
+         <input class="checkbox" type="checkbox" name="" id="">
+         <label class="fw-semibold" for="">3D Panel</label>
+        </div>
+        
+  `
+  showbtn.innerHTML = `Show More <i class="fa fa-arrow-up"></i>`
+ }
+});
+
